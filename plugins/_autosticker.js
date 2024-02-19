385e595bea59834a43eef290dff7878d89b94b00
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-19 12:01:13 +0100

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -17,7 +17,7 @@ handler.all = async function(m) {
       stiker = await sticker(img, false, packname, author);
     } else if (/video/g.test(mime)) {
       if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply(`_*< FUNCIONES - AUTOSTICKER />*_\n\n*[ ℹ️ ] La duración del vídeo no puede superar los 7 segundos.*\n\n*[ 💡 ] Para desactivar la función de* _autosticker_ *envie el siguiente comando:* _/𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚊𝚞𝚝𝚘𝚜𝚝𝚒𝚌𝚔𝚎𝚛_`);
-      // await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 7 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
+       await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 7 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
       const img = await q.download();
       if (!img) return;
       stiker = await sticker(img, false, packname, author);
