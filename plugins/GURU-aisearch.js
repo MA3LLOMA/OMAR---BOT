c284eb72454c3a0e36b512d4cf199f577a571a81
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-19 21:00:37 +0100

    Update GURU-aisearch.js

--- plugins/GURU-aisearch.js
+++ plugins/GURU-aisearch.js
@@ -1,52 +1,22 @@
-import fetch from 'node-fetch';
-import displayLoadingScreen from '../lib/loading.js'
-
-const endpoint = 'https://v2-guru-indratensei.cloud.okteto.net/perplexity?query=';
-
-let handler = async (m, { text, conn, usedPrefix, command }) => {
-  try {
-    if (!text && !(m.quoted && m.quoted.text)) {
-      throw `Please provide some text or quote a message to get a response.`;
-    }
-
-    if (!text && m.quoted && m.quoted.text) {
-      text = m.quoted.text;
-    } else if (text && m.quoted && m.quoted.text) {
-      text = `${text} ${m.quoted.text}`;
-      if (m.quoted.text.includes('.aisearch')) {
-        text = text.replace('.aisearch', ''); // 
-      }
-    }
-    await displayLoadingScreen(conn, m.chat)
-    conn.sendPresenceUpdate('composing', m.chat);
-    let emsg = await conn.sendMessage(m.chat, {text: 'Thinking...'})
-    const prompt = encodeURIComponent(text);
-
-    const response = await fetch(endpoint + prompt);
-
-    if (!response.ok) {
-      throw `Received an error response from the server: ${response.status} - ${response.statusText}`;
-    }
-
-    const data = await response.json();
-    const result = data.response.trim(); 
-    await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key: emsg.key,
-          type: 14,
-          editedMessage: {
-            conversation: result 
-          }
-        }
-      }, {})
-  } catch (error) {
-    console.error('Error:', error);
-    m.reply(`An error occurred while processing your request. Please try again later.`);
-  }
+import { Configuration, OpenAIApi } from "openai";
+let handler = async (m, { conn, text }) => {
+  if (!text) throw "[!] Masukkan teks.";
+  let configuration = new Configuration({
+    apiKey: "org-JdI9LMccweiz0xTLkI188ZjK",
+  });
+  let openai = new OpenAIApi(configuration);
+  let response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: text,
+    temperature: 0,
+    max_tokens: 3000,
+    top_p: 1,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
+  });
+  m.reply(response.data.choices[0].text);
 };
-handler.help = ['aisearch']
-handler.tags = ['AI']
-handler.command = ['aisearch', 'ai2']; 
-
-
+handler.help = ["ai", "openai"];
+handler.tags = ["info", "fun"];
+handler.command = /^(ai2|openai2)$/i;
 export default handler;
