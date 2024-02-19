a4c18ca27dd2baaa4a48142b58559353902f1a73
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-19 20:48:02 +0100

    Update _openai-ai-logic.js

--- plugins/_openai-ai-logic.js
+++ plugins/_openai-ai-logic.js
@@ -6,7 +6,7 @@ var handler = async (m, {
  command
  }) => {
 if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa Kamu? `
-//Set Logic Disini 
+Set Logic Disini 
 let logic = 'Hai Saya Adalah BetaBotz-Md Bot Whatsapp Yang Dikembangkan Oleh Lann,Saya Bernama Betabotz-Md,Saya Dibuat Oleh Lann Dengan Penuh Kesempurnaan Yang Tiada Taraa,Jika Kamu Ingin Mencari Tau Lebih Dalam Tentang Ownerku Visit https://api.betabotz.org'
 await m.reply(wait)
   var js = await fetch(`https://api.betabotz.eu.org/api/search/openai-logic?text=${text}&logic=${logic}&apikey=${lann}`)
