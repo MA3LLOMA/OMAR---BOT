10cdca4aa24cbd0c706bd219582101a3c0f31f95
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-19 21:03:19 +0100

    Add files via upload

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,17 +1,19 @@
-import { webp2png } from '../lib/webp2mp4.js'
-
+import { webp2png } from "../lib/webp2mp4.js";
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `✳️ Reply to a sticker with :\n\n *${usedPrefix + command}*`
-    if (!m.quoted) throw notStickerMessage
-    const q = m.quoted || m
-    let mime = q.mediaType || ''
-    if (!/sticker/.test(mime)) throw notStickerMessage
-    let media = await q.download()
-    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ Here you have*', m)
-}
-handler.help = ['toimg <sticker>']
-handler.tags = ['sticker']
-handler.command = ['toimg', 'jpg', 'aimg'] 
+  const notStickerMessage = `Reply sticker with command *${
+    usedPrefix + command
+  }*`;
+  const q = m.quoted || m;
+  if (!q) throw notStickerMessage;
+  await m.reply(wait);
+  let mime = q.mediaType || "";
+  if (!/sticker/.test(mime)) throw notStickerMessage;
+  let media = await q.download(true);
+  let out = (await webp2png(media))?.result || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, "out.png", "*DONE (≧ω≦)ゞ*", m);
+};
+handler.help = ["toimg (reply)"];
+handler.tags = ["sticker"];
+handler.command = ["toimg"];
 
-export default handler
+export default handler;
