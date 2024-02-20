9672fde305fb12ad75c9d7afd727f61cf7d105c0
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-20 15:33:56 +0100

    Add files via upload

--- plugins/downloader-apk.js
+++ plugins/downloader-apk.js
@@ -1,36 +1,27 @@
-import fetch from 'node-fetch';
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*هاذا الأمر خاص بتنزيل التطبيقات* \n مثال :. apkdl whatsapp`;
-
-  try {
-    const apkId = encodeURIComponent(args.join(' '));
-    const response = await fetch(`https://vihangayt.me/download/apk?id=${apkId}`);
-    const data = await response.json();
-
-    if (data.status) {
-      const apkData = data.data;
-      const message = `
-*Informasi APK*
-Nama: ${apkData.name}
-Last Update: ${apkData.lastup}
-Package: ${apkData.package}
-Size: ${apkData.size}
-Icon: ${apkData.icon}
-Download Link: ${apkData.dllink}
-      `;
-      await conn.sendFile(m.chat, apkData.dllink, `${apkData.name}.apk`, message, m);
-    } else {
-      conn.reply(m.chat, 'أعتذر لم اجد هاذا التطبيق 🤕 ..', m);
-    }
-  } catch (error) {
-    console.error(error);
-    conn.reply(m.chat, 'واسمح لي أن أعرف إذا كان لديك أي أسئلة', m);
-  }
-};
-
-handler.help = ['apkdl']
+	if (!args[0]) throw `Ex: ${usedPrefix + command} https://play.google.com/store/apps/details?id=com.linecorp.LGGRTHN`
+	let res = await apkDl(args[0])
+	await m.reply('wait')
+	conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m })
+}
+handler.help = handler.alias = ['apkdl2']
 handler.tags = ['downloader']
-handler.command = /^(apkdl|downloadapk|apkdownload)$/i
+handler.command = /^(apkdl2)$/i
+handler.limit = true
+export default handler
 
-export default handler;
+async function apkDl(url) {
+	let res = await fetch('https://apk.support/gapi/index.php', {
+		method: 'post',
+		body: new URLSearchParams(Object.entries({ x: 'downapk', t: 1, google_id: url, device_id: '', language: 'en-US', dpi: 480, gl: 'SUQ=', model: '', hl: 'en', de_av: '', aav: '', android_ver: 5.1, tbi: 'arm64-v8a', country: 0, gc: undefined }))
+	})
+	let $ = cheerio.load(await res.text())
+	let fileName = $('div.browser > div.dvContents > ul > li > a').text().trim().split(' ')[0]
+	let download = $('div.browser > div.dvContents > ul > li > a').attr('href')
+	if (!download) throw 'Can\'t download the apk!'
+	let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type')
+	return { fileName, mimetype, download }
+}
