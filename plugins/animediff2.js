5060873e2628207f47e2e51e85d2dc6c0bc35ce6
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-20 22:59:23 +0100

    Update animediff2.js

--- plugins/animediff2.js
+++ plugins/animediff2.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
         let url = `https://aemt.me/v5/text2img?text=${text}`
 
-        await conn.sendFile(m.chat, await (await fetch(url)).buffer(), 'fubuki.jpg', wm, m)
+        await conn.sendFile(m.chat, await (await fetch(url)).buffer(), 'fubuki.jpg', تابع صانع البوت على إنستجرام❤️ \n www.instagram.com/ovmar_1, m)
         m.react(done)
 
     } catch (e) {
