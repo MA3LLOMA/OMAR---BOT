8a6985072e67f992c1d0b02fe932639a354896c2
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-21 00:52:56 +0100

    Update tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -1,84 +1,35 @@
-import FormData from "form-data"
-import Jimp from "jimp"
+import fs from 'fs';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
+import deepai from 'deepai';
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-  conn.hdr = conn.hdr ? conn.hdr : {}
-  if (m.sender in conn.hdr)
-    throw "Masih Ada Proses Yang Belum Selesai Kak, Silahkan Tunggu Sampai Selesai Yah >//<"
+deepai.setApiKey(`${global.deepai}`);
+
+let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || q.mediaType || ""
-  if (!mime)
-    throw `هاذا الأمر يقوم بتحسين جود الصورة قم باإرسال الصور وقم بالرد \n\n .hd \n .remini \n`
-  if (!/image\/(jpe?g|png)/.test(mime))
-    throw `Mime ${mime} tidak support`
-  else conn.hdr[m.sender] = true;
-  m.reply("Proses Kak...")
-  let img = await q.download?.()
-  let error
-  try {
-    const This = await processing(img, "enhance")
-    conn.sendFile(m.chat, This, "", "تابع صانع البوت في إنستجرام ❤️ \n\n https://www.instagram.com/ovmar_1", m)
-  } catch (er) {
-    error = true
-  } finally {
-    if (error) {
-      m.reply("Proses Gagal :(")
-    }
-    delete conn.hdr[m.sender]
-  }
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Gambarnya Mana?';
+  await m.reply(global.wait);
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = mime.split('/')[1]
+  img = Date.now() + '.' + img
+  fs.writeFileSync(`./${img}`, await q.download());
+  let form = new FormData()
+  form.append('image', fs.createReadStream(`./${img}`));
+  let resp = await fetch('https://api.deepai.org/api/torch-srgan', {
+    method: 'POST',
+    headers: {
+      'api-key': `${global.deepai}`,
+    },
+    body: form,
+  })
+  let data = await resp.json()
+  await conn.sendFile(m.chat, data.output_url, 'hd.jpg', 'Ini Kak Hasilnyaヾ(≧▽≦*)ᴏ', m)
+  fs.unlinkSync(`./${img}`)
 }
 
-handler.help = ['hd', 'remini']
+handler.help = ['enchane']
 handler.tags = ['ai']
-handler.command = /^(hd|remini)$/i
-
-handler.register = falss
-handler.limit = 5
-handler.disable = false
+handler.command = /^(hd|upscale)$/i
 
 export default handler
-
-async function processing(urlPath, method) {
-  return new Promise(async (resolve, reject) => {
-    let Methods = ["enhance"]
-    Methods.includes(method) ? (method = method) : (method = Methods[0]);
-    let buffer,
-      Form = new FormData(),
-      scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
-    Form.append("model_version", 1, {
-      "Content-Transfer-Encoding": "binary",
-      contentType: "multipart/form-data; charset=uttf-8",
-    });
-    Form.append("image", Buffer.from(urlPath), {
-      filename: "enhance_image_body.jpg",
-      contentType: "image/jpeg",
-    });
-    Form.submit(
-      {
-        url: scheme,
-        host: "inferenceengine" + ".vyro" + ".ai",
-        path: "/" + method,
-        protocol: "https:",
-        headers: {
-          "User-Agent": "okhttp/4.9.3",
-          Connection: "Keep-Alive",
-          "Accept-Encoding": "gzip",
-        },
-      },
-      function (err, res) {
-        if (err) reject();
-        let data = [];
-        res
-          .on("data", function (chunk, resp) {
-            data.push(chunk);
-          })
-          .on("end", () => {
-            resolve(Buffer.concat(data));
-          });
-        res.on("error", (e) => {
-          reject();
-        });
-      }
-    );
-  });
-      }
