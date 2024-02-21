59a1fc6b1d270fef99264f16e9e01981c469148a
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-21 23:05:26 +0100

    Update GURU-qrmaker.js

--- plugins/GURU-qrmaker.js
+++ plugins/GURU-qrmaker.js
@@ -1,6 +1,6 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*Give a text to convert*`
+if (!text) throw `*هاذا الأمر مختص بتحويل الكتابة الى رمز QR \n\n exmaple: .qr  عمر بوت يرحب بكم*`
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'Here u go', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
