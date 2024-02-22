2449f1d37176e155a4004b0ad586cba278473d49
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-22 21:53:02 +0100

    Update ig-autodown.js

--- plugins/ig-autodown.js
+++ plugins/ig-autodown.js
@@ -4,12 +4,12 @@ const handler = async function (m, { conn }) {
     const args = m.text.split(' ');
     try {
         if (args.length > 0) {
-            m.reply('wait');
+            m.reply("wait");
             const url = args[0];
             let res = await (await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=GataDios&url=${url}`)).json(); 
 
             if (!res.result || res.result.length === 0) throw "لا يمكن العثور على الفيديو في الرابط";
-            conn.sendFile(m.chat, res.result[0], '', 'instagram.com/ovmar_1تابع صاحب البوت في إنستجرام \n', m);
+            conn.sendFile(m.chat, res.result[0], '', '*تابع صاحب البوت في الإنستجرام ❤️* \n https://www.instagram.com/ovmar_1', m);
 
             for (let imgs of res.result) {   
                 let ban = m.mentionedJid[0] || m.sender || conn.parseMention(args[0]) || (args[0].replace(/[@.+-]/g, '').replace(' ', '') + '@s.whatsapp.net') || '';
