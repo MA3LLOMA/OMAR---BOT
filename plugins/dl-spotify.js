17c09bdd06bc6bc46bb3c9f4a8f8568ebacee59d
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-22 14:24:10 +0100

    Add files via upload

---
+++ plugins/dl-spotify.js
@@ -0,0 +1,42 @@
+import fetch from 'node-fetch';
+import displayLoadingScreen from '../lib/loading.js';
+let handler = async (m, { conn, text }) => {
+    if (!text) {
+        console.log('No song name provided.');
+        throw `*قم باإرسال إسم الأغنية التي تريد* \n\n *مثال: .song Lmorphine*`;
+    }
+  m.react('🎶')
+  await displayLoadingScreen(conn, m.chat);
+  let pp = 'https://i.imgur.com/gzV8Pnk.jpg'
+    const query = encodeURIComponent(text);
+    let res = `https://guruapi.tech/api/spotifydl?url=${query}`
+   // let spotify = await (await fetch(res)).buffer()
+    let doc = {
+        audio: {
+          url: res
+        },
+        mimetype: 'audio/mpeg',
+        ptt: true,
+        waveform:  [100, 0, 100, 0, 100, 0, 100],
+        fileName: "Omar.mp3",
+    
+        contextInfo: {
+          mentionedJid: [m.sender],
+          externalAdReply: {
+            title: "MOROCCAN BOT",
+            body: `تستمع إلى: ${text}`,
+            thumbnailUrl: pp,
+            sourceUrl: null,
+            mediaType: 1,
+            renderLargerThumbnail: false
+          }
+        }
+    };
+    
+    await conn.sendMessage(m.chat, doc, { quoted: m });
+}
+handler.help = ['spotify'];
+handler.tags = ['downloader'];
+handler.command = /^(spotify|song)$/i;
+
+export default handler;
