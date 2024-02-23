3f89f0d69700613312cb2c5787aadac78290e84b
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-23 13:15:42 +0100

    Update dalle.js

--- plugins/dalle.js
+++ plugins/dalle.js
@@ -21,6 +21,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 };
 
 handler.help = ['dalle'];
-handler.tags = ['dalle'];
+handler.tags = ['drawing'];
 handler.command = ['dalle'];
 export default handler;
