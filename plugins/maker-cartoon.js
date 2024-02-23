a0f924a48720b1f88d9c60b15e44a5a1b2e35eb1
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-23 13:43:14 +0100

    Update maker-cartoon.js

--- plugins/maker-cartoon.js
+++ plugins/maker-cartoon.js
@@ -1,169 +1,31 @@
-import axios from "axios";
-import jimp from "jimp";
-import FormData from "form-data";
-import {
-    Sticker,
-    createSticker,
-    StickerTypes
-} from "wa-sticker-formatter";
+import fetch from 'node-fetch'
+import uploadImage from '../../lib/uploadImage.js'
+import uploadFile from '../../lib/uploadFile.js'
 
-async function GetBuffer(url) {
-    return new Promise(async (resolve, reject) => {
-        let buffer;
-        await jimp
-            .read(url)
-            .then((image) => {
-                image.getBuffer(image._originalMime, function(err, res) {
-                    buffer = res;
-                });
-            })
-            .catch(reject);
-        if (!Buffer.isBuffer(buffer)) reject(false);
-        resolve(buffer);
-    });
-}
-
-function GetType(Data) {
-    return new Promise((resolve, reject) => {
-        let Result, Status;
-        if (Buffer.isBuffer(Data)) {
-            Result = new Buffer.from(Data).toString("base64");
-            Status = 0;
-        } else {
-            Status = 1;
-        }
-        resolve({
-            status: Status,
-            result: Result,
-        });
-    });
-}
-async function Cartoon(url) {
-    return new Promise(async (resolve, reject) => {
-        let Data;
-        try {
-            let buffer = await GetBuffer(url);
-            let Base64 = await GetType(buffer);
-            await axios
-                .request({
-                    url: "https://access1.imglarger.com/PhoAi/Upload",
-                    method: "POST",
-                    headers: {
-                        connection: "keep-alive",
-                        accept: "application/json, text/plain, */*",
-                        "content-type": "application/json",
-                    },
-                    data: JSON.stringify({
-                        type: 11,
-                        base64Image: Base64.result,
-                    }),
-                })
-                .then(async ({
-                    data
-                }) => {
-                    let code = data.data.code;
-                    let type = data.data.type;
-                    while (true) {
-                        let LopAxios = await axios.request({
-                            url: "https://access1.imglarger.com/PhoAi/CheckStatus",
-                            method: "POST",
-                            headers: {
-                                connection: "keep-alive",
-                                accept: "application/json, text/plain, */*",
-                                "content-type": "application/json",
-                            },
-                            data: JSON.stringify({
-                                code: code,
-                                isMember: 0,
-                                type: type,
-                            }),
-                        });
-                        let status = LopAxios.data.data.status;
-                        if (status == "success") {
-                            Data = {
-                                message: "success",
-                                download: {
-                                    full: LopAxios.data.data.downloadUrls[0],
-                                    head: LopAxios.data.data.downloadUrls[1],
-                                },
-                            };
-                            break;
-                        } else if (status == "noface") {
-                            Data = {
-                                message: "noface",
-                            };
-                            break;
-                        }
-                    }
-                });
-        } catch (_error) {
-            Data = false;
-        } finally {
-            if (Data == false) {
-                reject(false);
-            }
-            resolve(Data);
-        }
-    });
-}
-
-function randomId() {
-    return Math.floor(100000 + Math.random() * 900000);
-}
 let handler = async (m, {
     conn,
     usedPrefix,
-    command
+    command,
+    text
 }) => {
-    conn.cartoon = conn.cartoon ? conn.cartoon : {};
-    if (m.sender in conn.cartoon)
-        throw "There is still an unfinished process, my friend. Please wait until it's over. >//<";
-    let q = m.quoted ? m.quoted : m;
-    let mime = (q.msg || q).mimetype || q.mediaType || "";
-    if (!mime) throw `Where is the picture you want to convert to a cartoon?`;
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `file ${mime} not supported`;
-    else conn.cartoon[m.sender] = true;
-    m.reply("converting the picture to cartoon");
-    let img = await q.download?.();
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let name = await conn.getName(who)
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!mime) throw 'Kirim/Reply Gambar Dengan Caption .toanime'
+    m.reply(wait)
     try {
-        Cartoon(img).then(async (response) => {
-            if (response.message == "success") {
-                await conn.sendFile(
-                    m.chat,
-                    response.download.full,
-                    "",
-                    "The operation was successful♥  >//<",
-                    m
-                );
-                let name = await conn.getName(m.sender),
-                    sticker = new Sticker(response.download.head, {
-                        pack: global.packname,
-                        author: name,
-                        type: StickerTypes.FULL,
-                        categories: ["🤩", "🎉"],
-                        id: randomId(),
-                        quality: 100,
-                        background: "#00000000",
-                    });
-                conn.sendMessage(m.chat, await sticker.toMessage(), {
-                    quoted: m
-                });
-            } else {
-                m.reply(
-                    "Excuse me my friend, the picture does not reveal a face, please send a picture in which the face is exposed and visible."
-                );
-            }
-        });
-    } catch {
-        m.reply("Process failed :(");
-    } finally {
-        conn.cartoon[m.sender] ? delete conn.cartoon[m.sender] : false;
+        let media = await q.download()
+        let url = await uploadFile(media)
+        let hasil = `https://api.xyroinee.xyz/api/others/toanime?url=${url}&apikey=${global.xyro}`
+        conn.sendFile(m.chat, await (await fetch(hasil)).arrayBuffer(), 'error.jpg', 'Nih Kak, Maaf Kalau Hasilnya Tidak Sesuai Keinginan', m)
+    } catch (e) {
+        m.reply(eror)
     }
-};
-handler.help = ["cartoon"];
-handler.tags = ["ai"];
-handler.command = ["cartoon"];
-
-handler.premium = false
+}
+handler.help = ['toanimex']
+handler.tags = ['ai']
+handler.command = /^(jadianimex|toanimex)$/i
+handler.limit = true
 
 export default handler
