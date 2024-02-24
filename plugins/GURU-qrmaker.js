f8cd79bb4cc9f9035024f689ccd8e2c6a04ce600
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-24 07:31:06 +0100

    Update GURU-qrmaker.js

--- plugins/GURU-qrmaker.js
+++ plugins/GURU-qrmaker.js
@@ -4,6 +4,6 @@ if (!text) throw `*هاذا الأمر مختص بتحويل الكتابة ال
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'Here u go', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
-handler.tags = ['tools']
+handler.tags = ['قائمة الأداوات']
 handler.command = /^qr(code)?$/i
 export default handler
