cdc4764d9da65ba32b27bdb4a250ba995643e3ad
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-24 07:46:09 +0100

    Update Wall-Ronaldo.js

--- plugins/Wall-Ronaldo.js
+++ plugins/Wall-Ronaldo.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
 conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
-//conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+//conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], 'https://www.instagram.com/ovmar_1'm)}
 handler.help = ['cristianoronaldo', 'cr7']
-handler.tags = ['img']
+handler.tags = ['قائمة الصور']
 handler.command = /^(ronaldo|cr7)$/i
 export default handler
