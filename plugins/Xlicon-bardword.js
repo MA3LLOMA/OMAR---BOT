fd495b78c3f3de6655c4b2ea3ed248c30c6bd885
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-24 07:49:34 +0100

    Update Xlicon-bardword.js

--- plugins/Xlicon-bardword.js
+++ plugins/Xlicon-bardword.js
@@ -1,4 +1,4 @@
-const badWords = /(gandu|maderchod|bhosdike|bhosda|laud?a|chut?iya|maa ki chut|behenchod|behen ki chut|tatto ke saudagar|machar ki jhant|jhant? ka baal|Rand?i ka aulad|chuchi|booob?ie?s|to?lo?l|idiot|nigga|fuck|sex|dick|bitch|tits|bastard|asshole|a[su,w,yu])/i;
+const badWords = /(gandu|maderchod|bhosdike|bhosda|laud?a|chut?iya|maa ki chut|xnxx|behen ki chut|tatto ke saudagar|machar ki jhant|jhant? ka sex|Rand?i ka aulad|chuchi|booob?ie?s|to?lo?l|idiot|nigga|fuck|sex|dick|bitch|tits|bastard|asshole|a[su,w,yu])/i;
 
 export async function before(m, { isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
@@ -12,9 +12,9 @@ export async function before(m, { isAdmin, isBotAdmin }) {
             // Remove the participant from the group
             global.db.data.users[m.sender].warn += 1
             await this.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }})
-            await this.reply(m.chat, '⛔ *Bad words are forbidden by group admins.*', m)
+            await this.reply(m.chat, '⛔ *السب والشتم ممنوع في المجموعة.*', m)
         } else {
-            await this.reply(m.chat, '😬 *I\'m not an admin here!*', m)
+            await this.reply(m.chat, '😬 *أنا لست المسؤول هنا..*', m)
         }
     }
     return !0
