5ac9e349c88dd36829ae0eba9dea140d2a1783b6
MA3LLOMA <157899772+MA3LLOMA@users.noreply.github.com>
2024-02-24 09:55:03 +0100

    Update fun-flirt.js

--- plugins/fun-flirt.js
+++ plugins/fun-flirt.js
@@ -9,7 +9,7 @@ let handler  = async (m, { conn }) => {
   conn.sendMessage(m.chat, { text: guru, mentions: [m.sender] }, { quoted: m })
 }
 handler.help = ['flirt']
-handler.tags = ['fun']
+handler.tags = ['قائمة الترفيه']
 handler.command = /^(flirt)$/i
 
 export default handler
